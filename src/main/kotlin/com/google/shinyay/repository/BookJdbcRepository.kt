0ccf609bc33cfe927a4060860a22b35db9712a3c
shinyay <shinya.com@gmail.com>
2021-08-04 22:08:24 +0900

    Add Query with JdbcTemplate and RowMapper

--- src/main/kotlin/com/google/shinyay/repository/BookJdbcRepository.kt
+++ src/main/kotlin/com/google/shinyay/repository/BookJdbcRepository.kt
@@ -1,8 +1,15 @@
 package com.google.shinyay.repository
 
+import com.google.shinyay.entity.Book
+import com.google.shinyay.entity.BookRowMapper
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
+import java.sql.ResultSet
 
 @Repository
 class BookJdbcRepository(val jdbcTemplate: JdbcTemplate) {
+
+    fun findAllBooks(): MutableList<Book> {
+        return jdbcTemplate.query("select * from books", BookRowMapper())
+    }
 }
