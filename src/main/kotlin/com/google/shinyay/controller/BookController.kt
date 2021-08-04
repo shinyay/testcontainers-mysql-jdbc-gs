c377602d2933a0aa3d1ec1d396efc8b6ebf5c868
shinyay <shinya.com@gmail.com>
2021-08-04 22:11:36 +0900

    Add find all books api with GET

---
+++ src/main/kotlin/com/google/shinyay/controller/BookController.kt
@@ -0,0 +1,27 @@
+package com.google.shinyay.controller
+
+import com.google.shinyay.entity.Book
+import com.google.shinyay.service.BookService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1")
+class BookController(val service: BookService) {
+
+    @GetMapping("/books")
+    fun findAllBooks(): ResponseEntity<MutableList<Book>> {
+        return ResponseEntity.status(HttpStatus.OK).body(service.repository.findAllBooks())
+    }
+
+//    @GetMapping("/books/{isbn}")
+//    fun findBookByIsbn(@PathVariable isbn: String): ResponseEntity<List<Book>> {
+//        return ResponseEntity.status(HttpStatus.OK).body(service.findBookByIsbn(isbn))
+//    }
+//
+//    @PostMapping("/books")
+//    fun registerBook(@RequestBody book: Book): ResponseEntity<Book> {
+//        return ResponseEntity.status(HttpStatus.CREATED).body(service.repository.save(book))
+//    }
+}
