b7aede3a9a0273d878be28b9d33b3dd1a1fe2b26
shinyay <shinya.com@gmail.com>
2021-08-04 22:10:41 +0900

    Add Service class for repository

---
+++ src/main/kotlin/com/google/shinyay/service/BookService.kt
@@ -0,0 +1,20 @@
+package com.google.shinyay.service
+
+import com.google.shinyay.entity.Book
+import com.google.shinyay.logger
+import com.google.shinyay.repository.BookJdbcRepository
+import com.google.shinyay.repository.BookRepository
+import org.springframework.stereotype.Service
+
+@Service
+class BookService(val repository: BookJdbcRepository) {
+
+    fun findAllBooks(): MutableList<Book> {
+        return repository.findAllBooks()
+    }
+
+//    fun findBookByIsbn(isbn: String): MutableList<Book> {
+//        logger.info("[ISBN]: $isbn")
+//        return repository.findBookByIsbn(isbn)
+//    }
+}
