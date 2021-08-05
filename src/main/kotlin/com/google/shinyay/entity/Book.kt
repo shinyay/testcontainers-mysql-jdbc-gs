7b2a0323ddf6dd2cd80c6d3993ed5aef7d5f1737
shinyay <shinya.com@gmail.com>
2021-08-05 22:12:57 +0900

    Remove Entity annotation for JPA

--- src/main/kotlin/com/google/shinyay/entity/Book.kt
+++ src/main/kotlin/com/google/shinyay/entity/Book.kt
@@ -1,12 +1,6 @@
 package com.google.shinyay.entity
 
-import javax.persistence.*
-
-@Entity
-@Table(name = "books")
 data class Book(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long,
     var isbn: String,
     var title: String,
