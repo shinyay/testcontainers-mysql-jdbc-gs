c2b749d7dbab92b6714aeeb23d959f32ae60cb9f
shinyay <shinya.com@gmail.com>
2021-08-05 22:13:48 +0900

    Add logger to extend Any as property

---
+++ src/main/kotlin/com/google/shinyay/TestcontainersMysqlJpaGsApplication.kt
@@ -0,0 +1,16 @@
+package com.google.shinyay
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestcontainersMysqlJpaGsApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestcontainersMysqlJpaGsApplication>(*args)
+}
+
+val Any.logger: Logger
+	get() = LoggerFactory.getLogger(this::class.java)
