e5c9e8706b57f50b6bfd20092d55953b9ca92f02
shinyay <shinya.com@gmail.com>
2021-08-08 21:48:49 +0900

    Modify import to optimize

--- src/test/kotlin/com/google/shinyay/unit/RepositoryTest.kt
+++ src/test/kotlin/com/google/shinyay/unit/RepositoryTest.kt
@@ -8,13 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.context.annotation.Import
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
-import org.testcontainers.containers.GenericContainer
-import org.testcontainers.containers.MySQLContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
-import org.testcontainers.utility.DockerImageName
 
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
